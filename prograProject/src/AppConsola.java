ca06cf740e01cd802e2a98534c0e17ea970cd6bd
Iulian <93289638+IulianSebastian@users.noreply.github.com>
2024-01-04 21:13:30 +0100

    Update AppConsola.java

--- prograProject/src/AppConsola.java
+++ prograProject/src/AppConsola.java
@@ -63,7 +63,84 @@ public class UsaSetmana {
             System.out.println("Quina opció vol utilitzar?");
             
         }
+    private static void ficarLlistaEntitats(ArxiuNormal entitats,LlistaEntitats llistaEntitats){
+        try{
+            String[] dades = entitats.LlegirArxiu();
+            int i = 0;
+            while(dades[i] != null){
+                String[] dadesFinals = dades[i].split(";");
+                String nom = dadesFinals[0];
+                String telefon = dadesFinals[1];
+                String adreca = dadesFinals[2];
+                Entitats entitat = new Entitats(nom,telefon,adreca);
+                llistaEntitats.afegirEntitat(entitat);
+                i++;
+            }
 
+        }catch(FileNotFoundException e){
+            System.out.println("Fitxer (entitats.txt) no trobat");
+        }catch(IOException e){
+            System.out.println("Un altre error");
+        }
 
-    
+    }
+    private static void ficarLlistaUsuaris(ArxiuNormal usuariFitxer,LlistaUsuaris llistaUsuaris){
+        try{
+            String[] dades = usuariFitxer.LlegirArxiu();
+            int i = 0;
+            while(dades[i] != null){
+                String[] dadesFinals = dades[i].split(";");
+                String alies = dadesFinals[0];
+                String correu = dadesFinals[1];
+                int codiPostal = Integer.parseInt(dadesFinals[2]);
+                Usuaris usuari = new Usuaris(alies,correu,codiPostal);
+                llistaUsuaris.afegirUsuari(usuari);
+                i++;
+            }
+
+        }catch(FileNotFoundException e){
+            System.out.println("Fitxer (usuaris.txt) no trobat");
+        }catch(IOException e){
+            System.out.println("Un altre error");
+        }
+
+    }
+    private static void ficarLlistaActivitats(ArxiuNormal activitatsFitxer,LlistaActivitats llistaActivitats){
+        try{
+            String[] dades = activitatsFitxer.LlegirArxiu();
+            int i = 0;
+            while(dades[i] != null){
+                String[] dadesFinals = dades[i].split(";");
+                String nom = dadesFinals[0];
+                String lloc = dadesFinals[1];
+                int codiPostal = Integer.parseInt(dadesFinals[2]);
+                String[] dataInici = dadesFinals[3].split("/");
+                Date data = new Date((Integer.parseInt(dataInici[0])-1900),(Integer.parseInt(dataInici[1])-1),Integer.parseInt(dataInici[2]));
+                String nomEntitat = dadesFinals[4];
+                try{
+                    int durada = Integer.parseInt(dadesFinals[5]);
+                    int capacitat = Integer.parseInt(dadesFinals[6]);
+                    Tallers talleria = new Tallers(nom,lloc,codiPostal,data,nomEntitat,durada,capacitat);
+                    llistaActivitats.afegirActivitat(talleria);
+                }catch(NumberFormatException e){
+                    String dada = dadesFinals[5];
+                    try{
+                        String serveis = dadesFinals[6];
+                        boolean adaptatCegos = Boolean.parseBoolean(dadesFinals[7]);
+                        Visites visita = new Visites(nom,lloc,codiPostal,data,nomEntitat,dada,serveis,adaptatCegos);
+                        llistaActivitats.afegirActivitat(visita);
+                    }catch(ArrayIndexOutOfBoundsException e2){
+                        Xerrades xerrada = new Xerrades(nom,lloc,codiPostal,data,nomEntitat,dada);
+                        llistaActivitats.afegirActivitat(xerrada);
+                    }
+                }
+                i++;
+            }
+        }catch(FileNotFoundException e){
+            System.out.println("Fitxer (activitats.txt) no trobat");
+        }catch(IOException e){
+            System.out.println("Un altre error");
+        }
+
+    }    
 }
