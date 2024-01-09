329cee989fed0f3cbc525c2646a1beecd403d167
Iulian <93289638+IulianSebastian@users.noreply.github.com>
2024-01-09 20:39:28 +0100

    Actualitzacio

--- prograProject/src/ArxiuNormal.java
+++ prograProject/src/ArxiuNormal.java
@@ -1,18 +1,20 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class ArxiuNormal{
 
-    String nom;
+    String ruta;
 
     public ArxiuNormal(String nom){
-        this.nom = nom;
+        ruta = "prograProject/src/arxius/"+nom;
     }
 
     public String[] LlegirArxiu() throws IOException{
         String[] dades = new String[99];
-        BufferedReader lectura = new BufferedReader(new FileReader(nom));
+        BufferedReader lectura = new BufferedReader(new FileReader(ruta));
         String frase = "";
         frase = lectura.readLine();
         int i = 0;
@@ -24,4 +26,33 @@ public class ArxiuNormal{
         lectura.close();
         return dades;
     }
+
+    public void EscriureArxiu(String[] dades,int numDades) throws IOException{
+
+        BufferedWriter escritura = new BufferedWriter (new FileWriter(ruta));
+
+        boolean seguir = true;
+        int i = 0;
+        while (seguir){
+            try{
+                if (dades[i] != null){
+                for(int j = i; j < (numDades+i); j++) {
+                    escritura.write(dades[j]);
+                    if (j != (numDades+i - 1)) {
+                        escritura.write(";");
+                    }else{
+                        escritura.write("\n");
+                    }    
+                }
+                i+=numDades;  
+            }
+            }catch(NullPointerException e){
+                seguir = false;
+            }catch(ArrayIndexOutOfBoundsException e){
+                seguir = false;
+            }
+        }
+        escritura.close();
+    }
 }
+
